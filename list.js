59300abd1ae82e71f6ce668ae905e0a2b22eb2f3
Mariusz Nowak <mariusz@medikoo.com>
2013-03-21 14:48:36 +0100

    `list` extension for domjs

---
+++ list.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var flatten   = require('es5-ext/lib/Array/prototype/flatten')
+  , extend    = require('es5-ext/lib/Object/extend')
+  , callable  = require('es5-ext/lib/Object/valid-callable')
+  , value     = require('es5-ext/lib/Object/valid-value')
+  , memoize   = require('memoizee/lib/regular')
+  , normalize = require('dom-ext/lib/Document/prototype/normalize')
+  , remove    = require('dom-ext/lib/Element/prototype/remove')
+
+  , map = Array.prototype.map
+  , List;
+
+List = function (domjs, list, cb, thisArg) {
+	var df;
+	this.domjs = domjs;
+	this.list = list;
+	this.thisArg = thisArg;
+	this.normalize = normalize.bind(domjs.document);
+	this.location = domjs.document.createTextNode("");
+	this.cb = cb;
+	if (typeof list.on === 'function') {
+		this.buildItem = memoize(this.buildItem.bind(this), { length: 1 });
+		list.on('change', this.reload.bind(this));
+	}
+	this.current = this.build();
+	df = domjs.document.createDocumentFragment();
+	this.current.forEach(df.appendChild, df);
+	df.appendChild(this.location);
+	return df;
+};
+
+extend(List.prototype, {
+	build: function () {
+		return flatten.call(map.call(this.list, function (item, index) {
+			return this.buildItem(item, index);
+		}, this));
+	},
+	buildItem: function (item, index) {
+		return this.domjs.safeCollect(this.cb.bind(this.thisArg, item, index,
+			this.list));
+	},
+	reload: function () {
+		this.current.forEach(function (el) { remove.call(el); });
+		this.current = this.build();
+		this.current.forEach(function (el) {
+			this.parentNode.insertBefore(el, this);
+		}, this.location);
+	}
+});
+
+module.exports = function (domjs) {
+	return function (list, cb/*, thisArg*/) {
+		return new List(domjs, value(list), callable(cb), arguments[2]);
+	};
+};
