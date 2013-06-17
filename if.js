85ad7e26f095f34b429d39c47352665b64bfa081
Mariusz Nowak <mariusz@medikoo.com>
2013-06-17 22:17:05 +0200

    Fix if for elements

--- if.js
+++ if.js
@@ -4,27 +4,32 @@ var d          = require('es5-ext/lib/Object/descriptor')
   , isFunction = require('es5-ext/lib/Function/is-function')
   , isMutable  = require('mutable/is')
   , normalize  = require('dom-ext/lib/Document/prototype/normalize')
-  , removeEl   = require('dom-ext/lib/Element/prototype/remove')
+  , removeNode = require('dom-ext/lib/Node/prototype/remove')
+  , isNode     = require('dom-ext/lib/Node/is-node')
 
   , defineProperty = Object.defineProperty
   , DOM, Attr;
 
 DOM = function (document, value, onTrue, onFalse) {
 	var df = document.createDocumentFragment(), current;
+	this.document = document;
 	if (!isMutable(value)) {
 		this.normalize(value ? onTrue : onFalse).forEach(df.appendChild, df);
 		return df;
 	}
 	this.dom = document.createTextNode("");
 	this.onTrue = onTrue;
+	if (isNode(onTrue)) removeNode.call(onTrue);
 	this.onFalse = onFalse;
+	if (isNode(onFalse)) removeNode.call(onFalse);
+
 	value.on('change', function (value) {
 		var nu, old, container;
 		value = Boolean(value);
 		if (value === current) return;
 		old = value ? this.false : this.true;
 		nu = value ? this.true : this.false;
-		old.forEach(function (el) { removeEl.call(el); });
+		old.forEach(function (el) { removeNode.call(el); });
 		container = this.dom.parentNode;
 		current = value;
 		if (!container) return;
@@ -37,7 +42,7 @@ DOM = function (document, value, onTrue, onFalse) {
 };
 Object.defineProperties(DOM.prototype, {
 	resolve: d(function (value) {
-		return normalize(isFunction(value) ? value() : value);
+		return normalize.call(this.document, isFunction(value) ? value() : value);
 	}),
 	true: d.gs(function () {
 		defineProperty(this, 'true', d(this.resolve(this.onTrue)));
