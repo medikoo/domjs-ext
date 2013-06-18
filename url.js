16acdb3d33bab5d7f84ad88a9634ff8a81db5fff
Mariusz Nowak <mariusz@medikoo.com>
2013-06-18 12:56:24 +0200

    Improve url handler

--- url.js
+++ url.js
@@ -2,16 +2,26 @@
 
 var arrayFrom = require('es5-ext/lib/Array/from')
 
-  , join = Array.prototype.join;
+  , forEach = Array.prototype.forEach;
 
 module.exports = function (domjs) {
 	return function (/*,  …paths*/) {
-		var paths = arguments;
+		var paths = arguments, base, query, hash, path;
 		if (this) {
 			paths = arrayFrom(paths);
 			paths.unshift(this);
 		}
 		if (!paths.length) return '/';
-		return '/' + join.call(paths, '/') + '/';
+		base = [];
+		query = [];
+		hash = '';
+		forEach.call(paths, function (path) {
+			if (path[0] === '?') query.push(path.slice(1));
+			else if (path[0] === '#') hash = path;
+			else base.push(path);
+		});
+		path = base.length ? ('/' + base.join('/') + '/') : '/';
+		if (query.length) path += '?' + query.join('&');
+		return path + hash;
 	};
 };
