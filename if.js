ce2429af1e0df41432543c9a6fea414a9ca26f06
Mariusz Nowak <mariusz@medikoo.com>
2013-07-19 18:40:49 +0200

    Update `if` up to changes in dom-ext

--- if.js
+++ if.js
@@ -5,14 +5,15 @@ var toArray    = require('es5-ext/lib/Array/from')
   , isFunction = require('es5-ext/lib/Function/is-function')
   , isMutable  = require('mutable/is')
   , normalize  = require('dom-ext/lib/Document/prototype/normalize')
+  , isDF       = require('dom-ext/lib/DocumentFragment/is-document-fragment')
   , removeNode = require('dom-ext/lib/Node/prototype/remove')
   , isNode     = require('dom-ext/lib/Node/is-node')
 
-  , defineProperty = Object.defineProperty
+  , defineProperty = Object.defineProperty, isArray = Array.isArray
   , DOM, Attr;
 
 DOM = function (document, value, onTrue, onFalse) {
-	var df, current;
+	var df, current, dom;
 	this.document = document;
 	if (!isMutable(value)) return this.resolve(value ? onTrue : onFalse);
 	df = document.createDocumentFragment();
@@ -28,14 +29,32 @@ DOM = function (document, value, onTrue, onFalse) {
 		if (value === current) return;
 		old = value ? this.false : this.true;
 		nu = value ? this.true : this.false;
-		old.forEach(function (el) { removeNode.call(el); });
+		if (old) {
+			if (isArray(old)) {
+				old.forEach(function (el) { removeNode.call(el); });
+			} else {
+				removeNode.call(old);
+			}
+		}
 		container = this.dom.parentNode;
 		current = value;
 		if (!container) return;
-		nu.forEach(function (el) { container.insertBefore(el, this.dom); }, this);
+		if (nu) {
+			if (isArray(nu)) {
+				nu.forEach(function (el) {
+					container.insertBefore(el, this.dom);
+				}, this);
+			} else {
+				container.insertBefore(nu, this.dom);
+			}
+		}
 	}.bind(this));
 	current = Boolean(value.value);
-	this[current ? 'true' : 'false'].forEach(df.appendChild, df);
+	dom = this[current ? 'true' : 'false'];
+	if (dom) {
+		if (isArray(dom)) dom.forEach(df.appendChild, df);
+		else df.appendChild(dom);
+	}
 	df.appendChild(this.dom);
 	return df;
 };
@@ -44,14 +63,22 @@ Object.defineProperties(DOM.prototype, {
 		return normalize.call(this.document, isFunction(value) ? value() : value);
 	}),
 	true: d.gs(function () {
-		defineProperty(this, 'true',
-			d(toArray(this.resolve(this.onTrue).childNodes)));
+		var dom = this.resolve(this.onTrue);
+		if (isDF(dom)) {
+			if (dom.childNodes.length) dom = toArray(dom.childNodes);
+			else dom = null;
+		}
+		defineProperty(this, 'true', d(dom));
 		delete this.onTrue;
 		return this.true;
 	}),
 	false: d.gs(function () {
-		defineProperty(this, 'false',
-			d(toArray(this.resolve(this.onFalse).childNodes)));
+		var dom = this.resolve(this.onFalse);
+		if (isDF(dom)) {
+			if (dom.childNodes.length) dom = toArray(dom.childNodes);
+			else dom = null;
+		}
+		defineProperty(this, 'false', d(dom));
 		delete this.onFalse;
 		return this.false;
 	})
