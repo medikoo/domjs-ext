83a3c5e133b4360b8e81b99d0fd7425879cd9d40
Mariusz Nowak <mariusz@medikoo.com>
2014-09-11 12:46:28 +0200

    Fix `fixed` directive internal logic

--- d/fixed.js
+++ d/fixed.js
@@ -2,10 +2,11 @@
 
 module.exports = function (domjs) {
 	domjs._directives._element.fixed = function () {
-		var element = this, style = element.style, isFixed;
+		var element = this, style = element.style, isFixed, triggerPos;
 		window.addEventListener('scroll', function () {
-			if (window.pageYOffset >= element.offsetTop) {
+			if (window.pageYOffset >= (isFixed ? triggerPos : element.offsetTop)) {
 				if (isFixed) return;
+				triggerPos = element.offsetTop;
 				style.position = 'fixed';
 				style.top = '0';
 				isFixed = true;
