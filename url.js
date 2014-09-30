ffa168ab6a3953c5aa03b3b7ab84d7db01ebb1d0
Mariusz Nowak <mariusz@medikoo.com>
2014-09-30 16:24:04 +0200

    Support observable values

--- url.js
+++ url.js
@@ -1,31 +1,40 @@
 'use strict';
 
-var aFrom = require('es5-ext/array/from')
+var aFrom           = require('es5-ext/array/from')
+  , ObservableValue = require('observable-value')
+  , isObservable    = require('observable-value/is-observable')
 
-  , forEach = Array.prototype.forEach;
+  , forEach = Array.prototype.forEach, resolve;
+
+resolve = function (paths) {
+	var base = [], query = [], hash = '', path;
+	forEach.call(paths, function (path) {
+		if (path == null) return;
+		path = String(path);
+		if (!path) return;
+		if (path[0] === '?') query.push(path.slice(1));
+		else if (path[0] === '#') hash = path;
+		else base.push(path);
+	});
+	path = base.length ? ('/' + base.join('/') + '/') : '/';
+	if (query.length) path += '?' + query.join('&');
+	return path + hash;
+};
 
 module.exports = function (domjs/*, options*/) {
 	var options = arguments[1];
 	domjs.ns[(options && options.name) || 'url'] = function (/*,  …paths*/) {
-		var paths = arguments, base, query, hash, path;
+		var paths = arguments, observable;
 		if (this) {
 			paths = aFrom(paths);
 			paths.unshift(this);
 		}
 		if (!paths.length) return '/';
-		base = [];
-		query = [];
-		hash = '';
 		forEach.call(paths, function (path) {
-			if (path == null) return;
-			path = String(path);
-			if (!path) return;
-			if (path[0] === '?') query.push(path.slice(1));
-			else if (path[0] === '#') hash = path;
-			else base.push(path);
+			if (!isObservable(path)) return;
+			if (!observable) observable = new ObservableValue(resolve(paths));
+			path.on('change', function () { observable.value = resolve(paths); });
 		});
-		path = base.length ? ('/' + base.join('/') + '/') : '/';
-		if (query.length) path += '?' + query.join('&');
-		return path + hash;
+		return observable || resolve(paths);
 	};
 };
