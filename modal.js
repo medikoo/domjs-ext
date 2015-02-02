bcd713bb49db2ee114f355682858901385e27af9
Mariusz Nowak <medyk@medikoo.com>
2015-02-02 10:47:08 +0100

    Additionally normalize dom

--- modal.js
+++ modal.js
@@ -4,10 +4,10 @@ var getFactory = require('overlay').getFactory;
 
 module.exports = function (domjs/*, options*/) {
 	var factory = getFactory(domjs.document), insert = domjs.ns.insert
-	  , options = arguments[1];
+	  , normalize = domjs.ns.normalize, options = arguments[1];
 	domjs.ns[(options && options.name) || 'modal'] = function (/* options */) {
 		var dom = factory.create.apply(factory, arguments);
-		insert(dom);
+		insert(normalize(dom));
 		return dom;
 	};
 };
