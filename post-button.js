03a8b98515d28384540b51914ffddb91b92abb90
kamsi <gruca.kamil@gmail.com>
2016-03-01 15:23:34 +0100

    #1 Enable observable in confirm

--- post-button.js
+++ post-button.js
@@ -6,7 +6,7 @@ var genId = require('time-uuid')
 
 module.exports = function (domjs/*, options*/) {
 	var form = domjs.ns.form, p = domjs.ns.p, button = domjs.ns.button
-	  , script = domjs.ns.script, options = arguments[1];
+	  , script = domjs.ns.script, options = arguments[1], mmap = domjs.ns.mmap;
 
 	domjs.ns[(options && options.name) || 'postButton'] = function (attrs) {
 		var id, confirm, message, value, buttonClass;
@@ -14,11 +14,6 @@ module.exports = function (domjs/*, options*/) {
 		if (!attrs.hasOwnProperty('method')) attrs.method = 'post';
 		if (!attrs.hasOwnProperty('id')) attrs.id = 'form-' + genId();
 		id = String(attrs.id);
-		if (attrs.confirm) {
-			confirm = true;
-			message = (attrs.confirm === true) ? null : String(attrs.confirm);
-			delete attrs.confirm;
-		}
 		buttonClass = attrs.buttonClass;
 		delete attrs.buttonClass;
 		value = attrs.value;
@@ -26,8 +21,13 @@ module.exports = function (domjs/*, options*/) {
 		return [form(attrs,
 			p(button({ type: 'submit', class: buttonClass }, value),
 				slice.call(arguments, 1))),
-			confirm && script(function (formId, message) {
-				$.confirmSubmit(formId, message);
-			}, id, message)];
+			mmap(attrs.confirm, function (message) {
+				if (!message) return;
+				if (message === true) message = null;
+				return script(function (formId, message) {
+					$.confirmSubmit(formId, message);
+				}, id, message);
+			})];
 	};
 };
+
